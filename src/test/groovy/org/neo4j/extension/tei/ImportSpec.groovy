62f25982d44a3ba3ef4fa900bc7320cc5474f03c
Stefan Armbruster <stefan@armbruster-it.de>
2016-04-11 11:46:02 +0200

    use graphviz to spit out generated graph in test

--- src/test/groovy/org/neo4j/extension/tei/ImportSpec.groovy
+++ src/test/groovy/org/neo4j/extension/tei/ImportSpec.groovy
@@ -2,8 +2,14 @@ package org.neo4j.extension.tei
 
 import org.junit.Rule
 import org.neo4j.extension.spock.Neo4jResource
+import org.neo4j.extension.spock.Neo4jUtils
 import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb.Label
 import org.neo4j.graphdb.factory.GraphDatabaseFactory
+import org.neo4j.helpers.collection.Iterables
+import org.neo4j.visualization.graphviz.GraphvizWriter
+import org.neo4j.walk.Visitor
+import org.neo4j.walk.Walker
 import spock.lang.Ignore
 import spock.lang.Specification
 
@@ -23,6 +29,7 @@ class ImportSpec extends Specification {
 
         when:
         def result = cut.importXml(this.class.getResourceAsStream("/1667-09-23_Langius_a_Lubienietzki-mit-Regest-ohne-Kommentar.xml"), null)
+//        writeGraphvizPngForLabel(Labels.Source)
 
         then:
         true
@@ -36,11 +43,32 @@ class ImportSpec extends Specification {
 
     }
 
+    private void writeGraphvizPngForLabel(Label label) {
+        def file = File.createTempFile("neo4j", ".dot")
+        Neo4jUtils.withTransaction(graphDatabaseService, {
+            new GraphvizWriter().emit(file, new Walker() {
+                @Override
+                def <R, E extends Throwable> R accept(Visitor<R, E> visitor) throws E {
+
+                    def sourceNodes = graphDatabaseService.findNodes(label)
+                    for (path in graphDatabaseService.traversalDescription().traverse(Iterables.asResourceIterable(sourceNodes))) {
+                        visitor.visitNode(path.endNode())
+                        path.endNode().relationships.each { visitor.visitRelationship(it) }
+                    }
+                    return visitor.done()
+                }
+            })
+        })
+        println "file is $file"
+        "dot -Tpng -O $file".execute()
+        "eog ${file}.png".execute()
+    }
+
     @Ignore
     def "populate test db"() {
 
         setup:
-        def targetDirectory = "/home/stefan/neo4j-enterprise-2.3.2/data/graph.db"
+        def targetDirectory = "${System.properties["user.home"]}/neo4j-enterprise-2.3.2/data/graph.db"
         new File(targetDirectory).deleteDir()
         GraphDatabaseService graphDatabaseService = new GraphDatabaseFactory().newEmbeddedDatabase(targetDirectory)
         def cut = new TeiImporter(graphDatabaseService: graphDatabaseService)
@@ -54,6 +82,7 @@ class ImportSpec extends Specification {
         cleanup:
         graphDatabaseService.shutdown()
     }
+
 
 }
 
